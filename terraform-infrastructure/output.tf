46dadfdec07a4e8f901514cd776e6f584363843b
Timna Brown <24630902+brown9804@users.noreply.github.com>
2025-03-20 15:06:40 -0600

    + output corrections more modules

--- terraform-infrastructure/output.tf
+++ terraform-infrastructure/output.tf
@@ -4,23 +4,49 @@ output "resource_group_name" {
 }
 
 output "storage_account_name" {
-  description = "The name of the storage account."
+  description = "The name of the storage account"
   value       = azurerm_storage_account.storage.name
 }
+
+output "input_container_name" {
+  description = "The name of the input container"
+  value       = azurerm_storage_container.input_container.name
+}
+
+output "output_container_name" {
+  description = "The name of the output container"
+  value       = azurerm_storage_container.output_container.name
+}
+
+output "function_app_name" {
+  description = "The name of the Linux Function App."
+  value       = azurerm_linux_function_app.function_app.name
+}
+
+output "app_service_plan_name" {
+  description = "The name of the Service Plan"
+  value       = azurerm_service_plan.asp.name
+}
+
+output "app_insights_name" {
+  description = "The name of the Application Insights instance"
+  value       = azurerm_application_insights.appinsights.name
+}
+
+output "log_analytics_workspace_name" {
+  description = "The name of the Log Analytics workspace"
+  value       = azurerm_log_analytics_workspace.loganalytics.name
+}
+
+output "key_vault_name" {
+  description = "The name of the Key Vault"
+  value       = azurerm_key_vault.keyvault.name
+}
+
 
 output "cosmosdb_account_name" {
   description = "The name of the CosmosDB account."
   value       = azurerm_cosmosdb_account.cosmosdb.name
-}
-
-output "app_service_name" {
-  description = "The name of the App Service."
-  value       = azurerm_app_service.app.name
-}
-
-output "key_vault_name" {
-  description = "The name of the Key Vault."
-  value       = azurerm_key_vault.kv.name
 }
 
 output "ai_service_name" {
@@ -30,5 +56,5 @@ output "ai_service_name" {
 
 output "openai_service_name" {
   description = "The name of the Azure OpenAI service."
-  value       = azurerm_openai_service.openai.name
+  value       = azurerm_cognitive_account.openai.name
 }
